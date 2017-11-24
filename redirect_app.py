3bacdfeb4768cd577c578fcf21cede83cb2dccb7
Daniel Bicho <daniel.bicho@fccn.pt>
2017-11-24 19:48:22 +0000

    Fix endereço de redirecionamento

--- redirect_app.py
+++ redirect_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,7 +9,8 @@ app.config.from_object('config')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirect(path):
-    return render_template('redirect.html', context="{}{}".format(app.config['WAYBACK_SERVER'], path))
+    redirect_url = "{}{}".format(app.config['WAYBACK_SERVER'], request.url)
+    return render_template('redirect.html', context=redirect_url)
 
 
 if __name__ == '__main__':
