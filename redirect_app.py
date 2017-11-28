ac8cc76d1d87873bb4d8dd6f5ca2be7e444f90cb
Daniel Bicho <daniel.bicho@fccn.pt>
2017-11-28 18:48:50 +0000

    Add specific archived version. rebranding home page.

--- redirect_app.py
+++ redirect_app.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -5,11 +7,19 @@ from flask import request
 app = Flask(__name__)
 app.config.from_object('config')
 
+# TODO change this and use the flask.g store
+global versions_dict
+versions_dict = defaultdict()
+versions_dict["www.umic.pt"] = "20170822151328"
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirect(path):
-    redirect_url = "{}{}".format(app.config['WAYBACK_SERVER'], request.url)
+    if versions_dict.get(request.host, None):
+        redirect_url = "{}{}/{}".format(app.config['WAYBACK_SERVER'], versions_dict[request.host], request.url)
+    else:
+        redirect_url = "{}{}".format(app.config['WAYBACK_SERVER'], request.url)
     return render_template('redirect.html', context=redirect_url)
 
 
