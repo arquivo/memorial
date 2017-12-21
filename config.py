890038175237d051be91d7db959ffb3399797b31
Daniel Bicho <daniel.bicho@fccn.pt>
2017-12-21 09:31:34 +0000

    move versions and template pick to config file

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
 WAYBACK_SERVER = 'http://arquivo.pt/wayback/'
+ARCHIVE_VERSIONS = {
+    "www.umic.pt": "20170822151328",
+    "umic.pt": "20170822151328",
+    "www.english.umic.pt": "20170831175908",
+    "english.umic.pt": "20170831175908"
+}
+TEMPLATES = {
+    "www.umic.pt": "redirect.html",
+    "umic.pt": "redirect.html",
+    "www.english.umic.pt": "redirectEN.html",
+    "english.umic.pt": "redirectEN.html"
+}
--- redirect_app.py
+++ redirect_app.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -7,21 +5,21 @@ from flask import request
 app = Flask(__name__)
 app.config.from_object('config')
 
-# TODO change this and use the flask.g store
-global versions_dict
-versions_dict = defaultdict()
-versions_dict["www.umic.pt"] = "20170822151328"
-versions_dict["www.english.umic.pt"] = "20170831175908"
-
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirect(path):
-    if versions_dict.get(request.host, None):
-        redirect_url = "{}{}/{}".format(app.config['WAYBACK_SERVER'], versions_dict[request.host], request.url)
+    if app.config['ARCHIVE_VERSIONS'].get(request.host, None):
+        redirect_url = "{}{}/{}".format(app.config['WAYBACK_SERVER'], app.config['ARCHIVE_VERSIONS'][request.host],
+                                        request.url)
     else:
         redirect_url = "{}{}".format(app.config['WAYBACK_SERVER'], request.url)
-    return render_template('redirect.html', context=redirect_url)
+
+    if app.config['TEMPLATES'].get(request.host, None):
+        template = app.config['TEMPLATES'][request.host]
+    else:
+        template = 'redirect.html'
+    return render_template(template, context=redirect_url)
 
 
 if __name__ == '__main__':
