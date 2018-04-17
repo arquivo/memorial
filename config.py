233646eab211776d557938ac4c2430258fc8733d
Daniel Bicho <daniel.bicho@fccn.pt>
2018-04-17 16:52:39 +0100

    default redirect page

--- config.py
+++ config.py
@@ -3,11 +3,23 @@ ARCHIVE_VERSIONS = {
     "www.umic.pt": "20170822151328",
     "umic.pt:": "20170822151328",
     "www.english.umic.pt": "20170831175908",
-    "english.umic.pt": "20170831175908"
+    "english.umic.pt": "20170831175908",
+    "www.gridcomputing.pt:5000": "20170803133931",
+    "gridcomputing.pt": "20170803133931",
+    "www.e-u.pt": "20171228124550",
+    "e-u.pt": "20171228124550",
+    "www.creativecommons.pt": "20170905135630",
+    "creativecommons.pt": "20170905135630"
 }
 TEMPLATES = {
     "www.umic.pt": "redirect.html",
     "umic.pt": "redirect.html",
     "www.english.umic.pt": "redirectEN.html",
-    "english.umic.pt": "redirectEN.html"
+    "english.umic.pt": "redirectEN.html",
+    "gridcomputing.pt": "redirect_default.html",
+    "www.gridcomputing.pt:5000": "redirect_default.html",
+    "www.e-u.pt": "redirect_default.html",
+    "e-u.pt": "redirect_default.html",
+    "creativecommons.pt": "redirect_default.html",
+    "www.creativecommons.pt": "redirect_default.html"
 }
