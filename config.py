0ce542cab7be6edbbabe9fdbd18aabc8fabb15b6
Ivo Branco <ivo.branco@fccn.pt>
2019-07-27 01:03:51 +0100

    Temporary commit config until google sheets sync isn't improved.

--- config.py
+++ config.py
@@ -3,19 +3,40 @@
 # It should be updated using update-from-sheets.sh script that is located on this repository base folder.
 
 ARCHIVE_CONFIG = {
-    'mundonaescola.pt': {'version': '20190315160356', 'message': 'O Mundo na Escola foi um programa de divulgação científica criado pelo Ministério da Educação e Ciência que funcionou entre abril de 2012 e novembro de 2015 com o objetivo de aproximar os profissionais das várias áreas do saber da comunidade escolar.'},
+    'mundonaescola.pt': {'version': '20190315160356', 'message': 'O Mundo na Escola foi um programa de divulgação científica criado pelo Ministério da Educação e Ciência que funcionou entre abril de 2012 e novembro de 2015 com o objetivo de aproximar os profissionais das várias áreas do saber da comunidade escolar.',
+        'button_color': '599bb3'
+    },
     'icolc.fccn.pt': {'version': '20180831131331'},
     'empregabilidadedigital.pt': {'version': '20180511135909'},
     'semplanos.com': {'version': '20180627151035'},
-    'visibilidade.net': {'version': '20180628130711'},
+    'visibilidade.net': {
+        'version': '20180628130711', 
+        'logo': 'https://arquivo.pt/wayback/20180628130712im_/http://visibilidade.net/img/logotipoVisibilidade.net.jpg',
+        'button_color': '01A6E0'
+    },
     'cidadesdigitais.pt': {'version': '20180430171300'},
     'cla.fccn.pt': {'version': '20180504071206'},
     'consultorioct.mctes.pt': {'version': '20180504071337'},
     'ipv6-tf.com.pt': {'version': '20180504071040'},
     'jeremias.com.pt': {'version': '20180504070838'},
     'ligarportugal.pt': {'version': '20170628163113'},
-    'english.umic.pt': {'version': '20170831175908', 'template': 'redirectEN.html'},
-    'umic.pt': {'version': '20170831104506', 'template': 'redirect.html'},
+    'english.umic.pt': {
+        'version': '20170831175908', 
+        'button_color': '599bb3',
+        'logo': '/static/img/umic.gif',
+        'default_language': 'en',
+        'message_en': ' UMIC - Knowledge Society Agency was a public institute that existed from January 2005 to February 2012, whose functions were transferred to <a href="https://www.fct.pt/dsi/index.phtml.en">FCT, I.P.</a>',
+        'link_pt': 'http://www.umic.pt'
+    },
+    #'umic.pt': {'version': '20170831104506', 'template': 'redirect.html', 'button_color': '599bb3'},
+    'umic.pt': {
+        'version': '20170831104506', 
+        'button_color': '599bb3',
+        'default_language': 'pt',
+        'logo': '/static/img/umic.gif',
+        'message_pt': 'A UMIC – Agência para a Sociedade do Conhecimento, foi um Instituto Público que existiu de janeiro de 2005 a fevereiro de 2012, cujas funções transitaram para a <a href="https://www.fct.pt/dsi/">FCT, I.P.</a>',
+        'link_en': 'http://www.english.umic.pt'
+    },
     'gridcomputing.pt': {'version': '20170803133931'},
     'e-u.pt': {'version': '20171228124550'},
 }
