4477033251d2fa1c31bbd8d5d19f975402049606
Ivo Branco <ivo.branco@fccn.pt>
2019-07-30 10:05:21 +0100

    Update configuration from google sheet.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ ARCHIVE_CONFIG = {
     'mundonaescola.pt': {
         'version': '20190315160356',
         'message_pt': 'O Mundo na Escola foi um programa de divulgação científica criado pelo Ministério da Educação e Ciência que funcionou entre abril de 2012 e novembro de 2015 com o objetivo de aproximar os profissionais das várias áreas do saber da comunidade escolar.',
-        'message_en': 'The Mundo na Escola was an outreach program of the Ministry of Education and Science that ran from April 2012 to November 2015 with the knowledge areas of the school community.',
+        'message_en': 'The Mundo na Escola was an outreach program of the Portuguese Ministry of Education and Science that ran from April 2012 to November 2015 to get closer to the knowledge areas of the school community.',
     },
     'icolc.fccn.pt': {
         'version': '20180831131331',
@@ -43,10 +43,18 @@ ARCHIVE_CONFIG = {
     'english.umic.pt': {
         'version': '20170831175908',
         'template': 'redirectEN.html',
+        'message_en': 'UMIC - Knowledge Society Agency was a public institute that existed from January 2005 to February 2012, whose functions were transferred to <a href=https://www.fct.pt/dsi/index.phtml.en>FCT, I.P.</a>',
+        'link_pt': 'http://www.umic.pt',
+        'logo': '/static/img/umic.gif',
+        'button_color': '599bb3',
     },
     'umic.pt': {
         'version': '20170831104506',
         'template': 'redirect.html',
+        'message_pt': 'A UMIC – Agência para a Sociedade do Conhecimento, foi um Instituto Público que existiu de janeiro de 2005 a fevereiro de 2012, cujas funções transitaram para a <a href=https://www.fct.pt/dsi/>FCT, I.P.</a>',
+        'link_en': 'http://www.english.umic.pt',
+        'logo': '/static/img/umic.gif',
+        'button_color': '599bb3',
     },
     'gridcomputing.pt': {
         'version': '20170803133931',
