d8faa8eedbab607c9b6c446aa5ffc043befce46b
Ivo Branco <ivo.branco@fccn.pt>
2019-12-18 14:42:44 +0000

    Change google sheets location

--- update-from-sheets.sh
+++ update-from-sheets.sh
@@ -9,18 +9,25 @@ correct(){
 }
 
 # Print first lines
-printHeaderComment() {
+printHeader() {
 	echo "# Managed by google sheets."
 	echo "# WARN do not update this file manually!"
 	echo "# It should be updated using update-from-sheets.sh script that is located on this repository base folder."
+	echo 
+	echo "ARCHIVE_CONFIG = {"
+}
+
+printFoolder() {
+	echo "}"
 }
 
 echo "Updating config"
 {
-	printHeaderComment
+	printHeader
 	echo
-	wget -qO- "https://docs.google.com/spreadsheets/d/1e4mmEdcIFR5kuFUGa1X2NaaK3qy0nu06m3oD3zDjp7w/export?format=csv&gid=1521923406&range=A5:A5" | tr -d '"'
-        echo
+	wget -qO- "https://docs.google.com/spreadsheets/d/1e4mmEdcIFR5kuFUGa1X2NaaK3qy0nu06m3oD3zDjp7w/export?format=csv&gid=0&range=AD2:AD" | sed '/^[[:space:]]*$/d' | tr -d '"'
+    printFoolder
+    echo
 } > config.py
 correct $? "Error updating config."
 
