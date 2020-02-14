107e88d6a3fbe81ad4698f33522865194f75ba1f
Ivo Branco <ivo.branco@fccn.pt>
2020-02-14 17:10:54 +0000

    Update default config.py with 8080 port the default when starting up on localhost for development.

--- config.py
+++ config.py
@@ -4,11 +4,19 @@
 
 ARCHIVE_CONFIG = {
 
-    'minema.di.fc.ul.pt': {
+    'senior3045.ipportalegre.pt:8080': {
+        'version': '20200117175504',
+        'message_pt': 'O programa <i>Fulbright Senior Specialist</i> sobre Apoios aos Alunos (<i>Support to Students</i>) decorreu no Instituto Politécnico de Portalegre em 2009 com a colaboração do Professor Katta G. Murty da Universidade do Michigan (EUA).',
+        'message_en': 'The program <i>Fulbright Senior Specialist</i> about <i>Support to Students</i> took place at the Instituto Politécnico de Portalegre in 2009 with the collaboration of Professor Katta G. Murty of the University of Michigan (USA).',
+        'logo': 'https://drive.google.com/uc?export=view&id=1axmlGNYfOHPReRk2gHDKrQFz78YlOWFE ',
+    },
+
+
+    'minema.di.fc.ul.pt:8080': {
         'version': '20191126170837',
     },
 
-    'knetworks.gov.pt': {
+    'knetworks.gov.pt:8080': {
         'version': '20190424165218',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'message_en': 'This page is digitally preserved by the Arquivo.pt, under a collaboration protocol with the Centro de Gestão da Rede Informática do Governo.',
@@ -16,14 +24,14 @@ ARCHIVE_CONFIG = {
         'button_color': '#777',
     },
 
-    'disaster-recovery.gov.pt': {
+    'disaster-recovery.gov.pt:8080': {
         'version': '20190424165203',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'logo': 'https://drive.google.com/uc?export=view&id=1haI3haNZmzPTxG---UCHjRfbFQu6M1xF',
         'button_color': '#777',
     },
 
-    'redesdoconhecimento.gov.pt': {
+    'redesdoconhecimento.gov.pt:8080': {
         'version': '20190424170738',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'message_en': 'This page is digitally preserved by the Arquivo.pt, under a collaboration protocol with the Centro de Gestão da Rede Informática do Governo.',
@@ -31,75 +39,75 @@ ARCHIVE_CONFIG = {
         'button_color': '#777',
     },
 
-    'diadomar.mdn.gov.pt': {
+    'diadomar.mdn.gov.pt:8080': {
         'version': '20061117092548',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'logo': 'https://drive.google.com/uc?export=view&id=1pAd9OLqATeczve6WZXfpdQLR7ENjnxm8',
         'button_color': '#777',
     },
 
-    'missaovenezuela.gov.pt': {
+    'missaovenezuela.gov.pt:8080': {
         'version': '20160211010406',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'logo': 'https://drive.google.com/uc?export=view&id=1RlB_LiYzc6z_kknDtLffiEhYR4XikwWz',
         'button_color': '#777',
     },
 
-    'religare.gov.pt': {
+    'religare.gov.pt:8080': {
         'version': '20191204111700',
         'message_pt': 'Esta página encontra-se preservada digitalmente pelo Arquivo.pt, ao abrigo de um protocolo de colaboração com o Centro de Gestão da Rede Informática do Governo.',
         'logo': 'https://drive.google.com/uc?export=view&id=1CxVDi1O-6RUi_jGpAlgHszg2yvmrqfnP',
         'button_color': '#777',
     },
 
-    'mundonaescola.pt': {
+    'mundonaescola.pt:8080': {
         'version': '20190315160356',
         'default_language': 'pt',
         'message_pt': 'O Mundo na Escola foi um programa de divulgação científica criado pelo Ministério da Educação e Ciência que funcionou entre abril de 2012 e novembro de 2015 com o objetivo de aproximar os profissionais das várias áreas do saber da comunidade escolar.',
         'message_en': 'The Mundo na Escola was an outreach program of the Portuguese Ministry of Education and Science that ran from April 2012 to November 2015 to get closer to the knowledge areas of the school community.',
     },
 
-    'icolc.fccn.pt': {
+    'icolc.fccn.pt:8080': {
         'version': '20180831131331',
     },
 
-    'empregabilidadedigital.pt': {
+    'empregabilidadedigital.pt:8080': {
         'version': '20180511135909',
     },
 
-    'semplanos.com': {
+    'semplanos.com:8080': {
         'version': '20180627151035',
     },
 
-    'visibilidade.net': {
+    'visibilidade.net:8080': {
         'version': '20180628130711',
     },
 
-    'cidadesdigitais.pt': {
+    'cidadesdigitais.pt:8080': {
         'version': '20180430171300',
     },
 
-    'cla.fccn.pt': {
+    'cla.fccn.pt:8080': {
         'version': '20180504071206',
     },
 
-    'consultorioct.mctes.pt': {
+    'consultorioct.mctes.pt:8080': {
         'version': '20180504071337',
     },
 
-    'ipv6-tf.com.pt': {
+    'ipv6-tf.com.pt:8080': {
         'version': '20180504071040',
     },
 
-    'jeremias.com.pt': {
+    'jeremias.com.pt:8080': {
         'version': '20180504070838',
     },
 
-    'ligarportugal.pt': {
+    'ligarportugal.pt:8080': {
         'version': '20170628163113',
     },
 
-    'english.umic.pt': {
+    'english.umic.pt:8080': {
         'version': '20170831175908',
         'default_language': 'en',
         'message_en': 'UMIC - Knowledge Society Agency was a public institute that existed from January 2005 to February 2012, whose functions were transferred to <a href=https://www.fct.pt/dsi/index.phtml.en>FCT, I.P.</a>',
@@ -108,7 +116,7 @@ ARCHIVE_CONFIG = {
         'button_color': '#599bb3',
     },
 
-    'umic.pt': {
+    'umic.pt:8080': {
         'version': '20170831104506',
         'default_language': 'pt',
         'message_pt': 'A UMIC – Agência para a Sociedade do Conhecimento, foi um Instituto Público que existiu de janeiro de 2005 a fevereiro de 2012, cujas funções transitaram para a <a href=https://www.fct.pt/dsi/>FCT, I.P.</a>',
@@ -117,11 +125,11 @@ ARCHIVE_CONFIG = {
         'button_color': '#599bb3',
     },
 
-    'gridcomputing.pt': {
+    'gridcomputing.pt:8080': {
         'version': '20170803133931',
     },
 
-    'e-u.pt': {
+    'e-u.pt:8080': {
         'version': '20171228124550',
     },
 }
